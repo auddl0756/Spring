e2f2c3be0e0f5b9fe51efbe1ac24481218ebc02c
auddl0756 <cjddl789123@naver.com>
2020-11-04 00:11:24 +0900

    .

--- 부품조립/entity/Exam.java
+++
@@ -1,6 +0,0 @@
-package 부품조립.entity;
-
-public interface Exam {
-	int total();
-	float average();
-}
--- 부품조립/src/부품조립/ui/ExamConsole.java
+++
@@ -1,9 +0,0 @@
-package 何前炼赋.ui;
-
-import 何前炼赋.entity.Exam;
-
-public interface ExamConsole {
-	void print();
-
-	void setExam(Exam exam);
-}
