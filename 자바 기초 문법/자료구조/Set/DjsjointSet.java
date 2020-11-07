8f3bf680934258a62d9a5da9013e7571456feca3
auddl0756 <cjddl789123@naver.com>
2020-11-07 23:18:22 +0900

    disjoint set class

---
+++ 자바 기초 문법/자료구조/Set/DjsjointSet.java
@@ -0,0 +1,29 @@
+package basic;
+
+class DjsjointSet {
+	final int MAX;
+	int[] parent;
+	
+	public DjsjointSet() {
+		this.MAX=10000;
+		parent =new int[MAX];
+		for(int i=0;i<MAX;i++) {
+			parent[i]=i;
+		}
+	}
+	
+	public int find(int idx) {
+		if(idx==parent[idx]) return idx;
+		return parent[idx] = find(parent[idx]);	//경로 압축
+	}
+	
+	public void merge(int idx1,int idx2) {
+		int p1,p2;
+		p1=find(idx1);
+		p2=find(idx2);
+		
+		if(p1==p2) return;
+		
+		parent[p1]=p2;
+	}
+}
