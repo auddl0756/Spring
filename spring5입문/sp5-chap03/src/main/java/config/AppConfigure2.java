a1814886669c4ad5e12d0ffca51cc2e152cff4e8
leemr <cjddl789123@naver.com>
2021-01-25 18:14:00 +0900

    2개의 설정 파일

---
+++ spring5입문/sp5-chap03/src/main/java/config/AppConfigure2.java
@@ -0,0 +1,48 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import spring.ChangePaswordService;
+import spring.MemberDao;
+import spring.MemberInfoPrinter;
+import spring.MemberListPrinter;
+import spring.MemberPrinter;
+import spring.MemberRegisterService;
+
+@Configuration
+public class AppConfigure2 {
+	@Autowired
+	private MemberDao memberDao;
+	
+	@Autowired
+	private MemberPrinter memberPrinter;
+	
+	@Bean
+	public MemberRegisterService memberRegSvc() {
+		return new MemberRegisterService(memberDao);
+	}
+	
+	@Bean
+	public ChangePaswordService changePwdSvc() {
+		ChangePaswordService pwdSvc = new ChangePaswordService();
+		pwdSvc.setMemberDao(memberDao);
+		return pwdSvc;
+	}
+	
+	@Bean
+	public MemberListPrinter listPrinter() {
+		return new MemberListPrinter(memberDao, memberPrinter);
+	}
+	
+	@Bean
+	public MemberInfoPrinter infoPrinter() {
+		MemberInfoPrinter infoPrinter =new MemberInfoPrinter();
+		infoPrinter.setMemberDao(memberDao);
+		infoPrinter.setPrinter(memberPrinter);
+		return infoPrinter;
+	}
+	
+	
+}
