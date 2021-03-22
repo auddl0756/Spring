bac75f7d654671cf2db7edcbb66c51c849743140
auddl0756 <cjddl789123@naver.com>
2021-03-22 19:13:23 +0900

    정렬:Comparator or lambda

---
+++ 자바 기초 문법/자바로 문제풀이/자바 기초 문법/정렬.java
@@ -0,0 +1,48 @@
+package 정렬;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+//객체 정렬하기
+public class 정렬 {
+    public static void main(String[] args) {
+        정렬 sample = new 정렬();
+
+        List<Music> musics = new ArrayList<>();
+        //access via sample and create "Music" object
+        //else just make "Music" object exist using "static" keyword
+        musics.add(sample.new Music("classic",123));
+        musics.add(sample.new Music("pop",1234));
+        musics.add(sample.new Music("hip",12345));
+        musics.add(sample.new Music("ballad",123456));
+
+//        Collections.sort(musics,new Comparator<Music>(){
+//            @Override
+//            public int compare(Music o1, Music o2) {
+//                return o1.played-o2.played;
+//            }
+//        });
+
+        musics.sort((c1,c2)->c1.played-c2.played);
+
+        for(int i=0;i<4;i++){
+            System.out.println(musics.get(i).genre);
+        }
+    }
+
+    class Music{
+        String  genre;
+        int played;
+
+        Music(String genre,int played){
+            this.genre=genre;
+            this.played=played;
+        }
+    }
+}
+
+
+
+
